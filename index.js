4959b7540c9192c67ae7fe13181d8eae7d606e3a
admin <admin@MacBook-Pro-de-admin-2.local>
2025-09-22 07:15:53 -0500

    New architecture

--- index.js
+++ index.js
@@ -1,45 +1,28 @@
 const express = require("express");
 const path = require("path");
-const { Server } = require("socket.io");
 const { createServer } = require("http");
 
-const app = express();
+const usersRouter = require("./server/routes/users.router");
+const screen1EventsRouter = require("./server/routes/screen1Events.router");
+const { initSocketInstance } = require("./server/services/socket.service");
 
+const PORT = 5050;
+
+const app = express();
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, {
-  path: "/real-time",
-  cors: {
-    origin: "*",
-  },
-});
-
+// Middlewares
 app.use(express.json());
 app.use("/app1", express.static(path.join(__dirname, "app1")));
 app.use("/app2", express.static(path.join(__dirname, "app2")));
 
-let users = [
-  {
-    id: 1,
-    name: "John Doe",
-  },
-];
+// Routes
+app.use("/", usersRouter);
+app.use("/", screen1EventsRouter);
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+// Services
+initSocketInstance(httpServer);
 
-app.post("/users", (req, res) => {
-  const { id, name } = req.body;
-  users.push({ id, name });
-  res.send(users);
-});
-
-app.post("/change-screen", (req, res) => {
-  io.emit("next-screen");
-  res.send({ message: "Cambio de pantalla exitoso" });
-});
-
-httpServer.listen(5050, () =>
-  console.log(`Server running at http://localhost:${5050}`)
+httpServer.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
 );
